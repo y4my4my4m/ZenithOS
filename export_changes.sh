a34b164773f99f17a5fac4098199cc75d6635ef7
mintsuki <mintsuki@protonmail.com>
2020-06-10 20:27:43 +0200

    Fix image name in export_changes.sh

--- export_changes.sh
+++ export_changes.sh
@@ -8,7 +8,7 @@ sudo -v
 # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-sudo losetup -Pf --show ZenithOS.img > loopback_dev
+sudo losetup -Pf --show ZenithOS.hdd > loopback_dev
 sudo partprobe `cat loopback_dev`
 
 mkdir -p mnt
